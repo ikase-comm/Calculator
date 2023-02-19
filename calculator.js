c115eeeb2ef6299ad81032f4f8146222c8b3ffc8
ikase-comm <124385023+ikase-comm@users.noreply.github.com>
2023-02-19 15:37:36 +0200

    modified:   calculator.js

--- calculator.js
+++ calculator.js
@@ -24,6 +24,15 @@ app.get('/bmicalculator.html', function(req, res) {
     res.sendFile(__dirname + "/bmicalculator.html")
 })
 
+app.post("/bmicalculator.html", function(req, res) {
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+
+    var bmi = weight / (height * height);
+
+    res.send("Your BMI is " + bmi);
+})
+
 app.listen(3000, function(){
     console.log(`Server is running on port 3000`);
 });
